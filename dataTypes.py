a085045fe906d79964a6038ca5829dacd1e67aab
dakshsharma14 <dakshsharma0214@gmail.com>
2023-07-15 17:08:58 -0400

    First Commit

---
+++ dataTypes.py
@@ -0,0 +1,78 @@
+# Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# classes -> custom types
+
+# specialized Data Types
+
+
+# nothing
+None
+
+
+###################################
+# int and float
+print(3+7)
+print(2+4)
+print(type(4.1))
+print(type(4+7))
+# float takes up more space in memory because as it is
+# difficult to store binary numbers in the memory.
+print(2 ** 3)
+print(2 / 3)
+print(14 // 3)
+
+print(14 % 3)
+# modulus remainder
+
+# floating point numbers are scientific notation
+print(0.2+0.1)
+print(0.3+0.3+0.3)
+
+# math function
+print(round(3.1))
+# absolute value with no negative numbers
+print(abs(-20))
+
+
+# operator precedence
+print((30+2) - 2**3)
+
+# ()
+# **
+# * /
+# + -
+
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
+
+print((5 + 4) * 10 / 2)
+
+print(((5 + 4) * 10) / 2)
+
+print((5 + 4) * (10 / 2))
+
+print(5 + (4 * 10) / 2)
+
+print(5 + 4 * 10 // 2)
+
+# complex, used if we are doing complex math
+
+# return the binary version of number stored in memory
+print(bin(5))
+
+# or second one is the base 2
+print(int('0b101', 2 ))
+
+
+
+
+
+
